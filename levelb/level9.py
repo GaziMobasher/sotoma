85b359cc76bfcd0bfd8024dd01fa7a301c8cc737
Gazi Muhammad Mobasher <143348243+AD3OR@users.noreply.github.com>
2026-02-19 02:26:37 +0600

    constructed level 9
    as

---
+++ levelb/level9.py
@@ -0,0 +1,31 @@
+# Level 4
+import pygame
+from blockt import dblocks, gblocks, xblocks, sblocks
+from config import SCALE
+
+
+def load():
+    # Main static platform
+    platform = pygame.Rect(
+        int(800),
+        int(900),
+        int(200 * SCALE),
+        int(30 * SCALE)
+    )
+
+    # Rectangular draggable platforms
+    blocks = [
+        xblocks.create_xblock(300, 60),
+        gblocks.create_gblock(400, 120),
+        sblocks.create_sblock(160, 160)
+    ]
+
+    # Exit
+    exit_rect = pygame.Rect(
+        int(280 * SCALE),
+        int(1 * SCALE),
+        int(100 * SCALE),
+        int(100 * SCALE)
+    )
+
+    return platform, blocks, exit_rect
--- main.py
+++ main.py
@@ -50,7 +50,7 @@ back_button_rect = pygame.Rect(
 
 
 # -------------------- LEVEL STATE --------------------
-current_level = 8
+current_level = 9
 platform, blocks, exit_rect = levels.load_level(current_level)
 
 # Split blocks into separate lists
